508535042bb53db1f6465171095787cf3a003bef
Natalia Yakovleva <yakovlevane29@gmail.com>
2026-01-05 16:42:31 +0200

    fixed

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -10,8 +10,7 @@ export const PostInfo = ({ post }) => (
 
       <p>
         {' Posted by  '}
-
-        <UserInfo user={post.user} />
+        {post.user ? <UserInfo user={post.user} /> : null}
       </p>
     </div>
 
